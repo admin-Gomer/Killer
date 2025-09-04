afeb3d8899d80a8e5417bc76fbd84b1d39a35630
◉𝔾𝕠𝕞𝕖𝕣◉ <m270183v@gmail.com>
2025-09-05 07:03:29 +1000

    ꧁☠︎︎🅼🅸🅺🅷🅰︎🅸🅻☠︎︎꧂

--- commands/groups/on_off.js
+++ commands/groups/on_off.js
@@ -1,8 +1,8 @@
 module.exports = {
-  command: ["on", "off"],
-  description: "Activa o desactiva funciones del grupo",
-  category: "groups",
-  use: "antilink",
+  command: ["включить", "выключить", "on", "off"],
+  description: "Включение и отключение функций группы",
+  category: "Группы",
+  use: "антиссылка",
   isGroup: true,
   isAdmin: true,
   isBotAdmin: true,
@@ -11,22 +11,22 @@ module.exports = {
     const setting = args[0]?.toLowerCase();
     if (!setting) {
       return m.reply(
-        "Debes especificar la *función*\n\n`Ejemplo`\n!on antilink\n!off antilink",
+        "_*Необходимо указать функцию*_\n\n`Пример`\n_#включить антиссылка_\n_#выключить антиссылка_",
       );
     }
     const chatData = global.db.data.chats[m.chat];
 
     switch (setting) {
-      case "antilink":
-        chatData.antilink = cmd === "on";
+      case "антиссылка":
+        chatData.antilink = cmd === "включить";
         m.reply(
-          `La función *Antilink* ha sido *${cmd === "on" ? "activada" : "desactivada"}*`,
+          `Функция *антиссылка* была выполнена *${cmd === "включить" ? "Активированный" : "Нетрудоспособный"}*`,
         );
         break;
 
       default:
         m.reply(
-          "Opción no *válida*\n\n- Opciones:\n`antilink`\n\n\n> Ejemplo: .on antilink",
+          "_Опция не_ *действительна*\n\n- `Опции:`\n'антиссылка'\n\n\n> `Пример:` #включить антиссылка",
         );
         break;
     }
